2f965c559af8d3bcb111c48f6478d94214d454c9
roughlang <roughlangx@gmail.com>
2021-12-10 13:35:59 +0900

    header nav include

---
+++ app/public/ac/wp-content/themes/208/inc/nav.php
@@ -0,0 +1,40 @@
+<div class="topbar"></div>
+<nav class="navbar navbar-expand-lg navbar-light bg-white">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/"><?php bloginfo('name'); ?></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 nav208">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="/">Size & Price</a>
+        </li>
+				<li class="nav-item">
+          <a class="nav-link active" href="/">Attention</a>
+        </li>
+				<li class="nav-item">
+          <a class="nav-link active" href="#">Reservation</a>
+        </li>
+
+				<li class="nav-item dropdown active">
+          <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            link
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="https://www.instagram.com/natsuko208/?hl=ja" target="_blank">Instagram</a></li>
+            <li><a class="dropdown-item" href="https://twitter.com/Mehndi208" target="_blank">Twitter</a></li>
+            <li><a class="dropdown-item" href="#" target="_blank">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="d-flex search-form">
+        <input class="form-control me-2 search-input" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-secondary search-btn" type="submit"></button>
+      </form>
+    </div>
+  </div>
+</nav>
--- app/public/ac/wp-content/themes/208/index.php
+++ app/public/ac/wp-content/themes/208/index.php
@@ -1,102 +1,7 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="robots" content="index, follow, archive">
-	<title><?php bloginfo('name'); ?></title>
-	<meta name="description" content="天然染料で染める、2週間で消えるボディアートジャグアタトゥー &ヘナタトゥー。東京・JR国分寺駅徒歩1分。2週間で消えるジャグアタトゥーとは？">
-  <link rel="canonical" href="https://natural-body-art-208.com/">
-  <meta name="msapplication-square70x70logo" content="/assets/img/icon/favicon/site-tile-70x70.png">
-  <meta name="msapplication-square150x150logo" content="/assets/img/icon/favicon/site-tile-150x150.png">
-  <meta name="msapplication-wide310x150logo" content="/assets/img/icon/favicon/site-tile-310x150.png">
-  <meta name="msapplication-square310x310logo" content="/assets/img/icon/favicon/site-tile-310x310.png">
-  <meta name="msapplication-TileColor" content="#0078d7">
-  <link rel="shortcut icon" type="image/vnd.microsoft.icon" href="/assets/img/icon/favicon/favicon.ico">
-  <link rel="icon" type="image/vnd.microsoft.icon" href="/assets/img/icon/favicon/favicon.ico">
-  <link rel="apple-touch-icon" sizes="57x57" href="/assets/img/icon/favicon/apple-touch-icon-57x57.png">
-  <link rel="apple-touch-icon" sizes="60x60" href="/assets/img/icon/favicon/apple-touch-icon-60x60.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="/assets/img/icon/favicon/apple-touch-icon-72x72.png">
-  <link rel="apple-touch-icon" sizes="76x76" href="/assets/img/icon/favicon/apple-touch-icon-76x76.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="/assets/img/icon/favicon/apple-touch-icon-114x114.png">
-  <link rel="apple-touch-icon" sizes="120x120" href="/assets/img/icon/favicon/apple-touch-icon-120x120.png">
-  <link rel="apple-touch-icon" sizes="144x144" href="/assets/img/icon/favicon/apple-touch-icon-144x144.png">
-  <link rel="apple-touch-icon" sizes="152x152" href="/assets/img/icon/favicon/apple-touch-icon-152x152.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/icon/favicon/apple-touch-icon-180x180.png">
-  <link rel="icon" type="image/png" sizes="36x36" href="/assets/img/icon/favicon/android-chrome-36x36.png">
-  <link rel="icon" type="image/png" sizes="48x48" href="/assets/img/icon/favicon/android-chrome-48x48.png">
-  <link rel="icon" type="image/png" sizes="72x72" href="/assets/img/icon/favicon/android-chrome-72x72.png">
-  <link rel="icon" type="image/png" sizes="96x96" href="/assets/img/icon/favicon/android-chrome-96x96.png">
-  <link rel="icon" type="image/png" sizes="128x128" href="/assets/img/icon/favicon/android-chrome-128x128.png">
-  <link rel="icon" type="image/png" sizes="144x144" href="/assets/img/icon/favicon/android-chrome-144x144.png">
-  <link rel="icon" type="image/png" sizes="152x152" href="/assets/img/icon/favicon/android-chrome-152x152.png">
-  <link rel="icon" type="image/png" sizes="192x192" href="/assets/img/icon/favicon/android-chrome-192x192.png">
-  <link rel="icon" type="image/png" sizes="256x256" href="/assets/img/icon/favicon/android-chrome-256x256.png">
-  <link rel="icon" type="image/png" sizes="384x384" href="/assets/img/icon/favicon/android-chrome-384x384.png">
-  <link rel="icon" type="image/png" sizes="512x512" href="/assets/img/icon/favicon/android-chrome-512x512.png">
-  <link rel="icon" type="image/png" sizes="36x36" href="/assets/img/icon/favicon/icon-36x36.png">
-  <link rel="icon" type="image/png" sizes="48x48" href="/assets/img/icon/favicon/icon-48x48.png">
-  <link rel="icon" type="image/png" sizes="72x72" href="/assets/img/icon/favicon/icon-72x72.png">
-  <link rel="icon" type="image/png" sizes="96x96" href="/assets/img/icon/favicon/icon-96x96.png">
-  <link rel="icon" type="image/png" sizes="128x128" href="/assets/img/icon/favicon/icon-128x128.png">
-  <link rel="icon" type="image/png" sizes="144x144" href="/assets/img/icon/favicon/icon-144x144.png">
-  <link rel="icon" type="image/png" sizes="152x152" href="/assets/img/icon/favicon/icon-152x152.png">
-  <link rel="icon" type="image/png" sizes="160x160" href="/assets/img/icon/favicon/icon-160x160.png">
-  <link rel="icon" type="image/png" sizes="192x192" href="/assets/img/icon/favicon/icon-192x192.png">
-  <link rel="icon" type="image/png" sizes="196x196" href="/assets/img/icon/favicon/icon-196x196.png">
-  <link rel="icon" type="image/png" sizes="256x256" href="/assets/img/icon/favicon/icon-256x256.png">
-  <link rel="icon" type="image/png" sizes="384x384" href="/assets/img/icon/favicon/icon-384x384.png">
-  <link rel="icon" type="image/png" sizes="512x512" href="/assets/img/icon/favicon/icon-512x512.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="/assets/img/icon/favicon/icon-16x16.png">
-  <link rel="icon" type="image/png" sizes="24x24" href="/assets/img/icon/favicon/icon-24x24.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/icon/favicon/icon-32x32.png">
-  <link rel="manifest" href="/assets/img/icon/favicon/manifest.json">
-
-	<link rel="stylesheet" href="/assets/css/_main.css" media="screen">
-	<script type="text/javascript" src="/assets/bootstrap5/js/bootstrap.min.js"></script>
-</head>
+<?php include("inc/html_header.php"); ?>
 <body>
-<div class="topbar"></div>
-<nav class="navbar navbar-expand-lg navbar-light bg-white">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/"><?php bloginfo('name'); ?></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0 nav208">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" href="/">Size & Price</a>
-        </li>
-				<li class="nav-item">
-          <a class="nav-link active" href="/">Attention</a>
-        </li>
-				<li class="nav-item">
-          <a class="nav-link active" href="#">Reservation</a>
-        </li>
 
-				<li class="nav-item dropdown active">
-          <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            link
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a class="dropdown-item" href="https://www.instagram.com/natsuko208/?hl=ja" target="_blank">Instagram</a></li>
-            <li><a class="dropdown-item" href="https://twitter.com/Mehndi208" target="_blank">Twitter</a></li>
-            <li><a class="dropdown-item" href="#" target="_blank">Something else here</a></li>
-          </ul>
-        </li>
-      </ul>
-      <form class="d-flex search-form">
-        <input class="form-control me-2 search-input" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-secondary search-btn" type="submit"></button>
-      </form>
-    </div>
-  </div>
-</nav>
+<?php include("inc/nav.php"); ?>
 <div class="p-3 p-sm-5 mb-4 border-top big-banner-top" id="big-banner-top">
   <div class="container">
     <h1 class="display-4">Natural body art 208</h1>
@@ -195,35 +100,23 @@
     </div>
   </div>
 </div>
-      <!-- <div class="col-4 col-sm-4 col-lg-2 price-image-block">
-        
-      </div>
-      <div class="col-4 col-sm-4 col-lg-2 price-image-block">
-        <img src="/assets/img/top/price/image-02.jpg" class="price-image" alt="jagua-image">
-      </div>
-      <div class="col-4 col-sm-4 col-lg-2 price-image-block">
-        <img src="/assets/img/top/price/image-03.jpg" class="price-image" alt="jagua-image">
-      </div>
-      <div class="col-4 col-sm-4 col-lg-2 price-image-block">
-        <img src="/assets/img/top/price/image-04.jpg" class="price-image" alt="jagua-image">
-      </div>
-      <div class="col-4 col-sm-4 col-lg-2 price-image-block">
-        <img src="/assets/img/top/price/image-05.jpg" class="price-image" alt="jagua-image">
-      </div>
-      <div class="col-4 col-sm-4 col-lg-2 price-image-block">
-        <img src="/assets/img/top/price/image-06.jpg" class="price-image" alt="jagua-image">
-      </div>
-      <div class="col-4 col-sm-4 col-lg-2 price-image-block">
-        <img src="/assets/img/top/price/image-07.jpg" class="price-image" alt="jagua-image">
-      </div>
-      <div class="col-4 col-sm-4 col-lg-2 price-image-block">
-        <img src="/assets/img/top/price/image-08.jpg" class="price-image" alt="jagua-image">
-      </div>
-      <div class="col-4 col-sm-4 col-lg-2 price-image-block">
-        <img src="/assets/img/top/price/image-09.jpg" class="price-image" alt="jagua-image">
-      </div> -->
     
-
+<div class="container top-block-02">
+  <div class="row">
+    <div class="col-sm-12">
+      <h2>Blog</h2>
+      <!--wordpress loop-->
+      <?php if(have_posts()): ?>
+        <?php while(have_posts()): the_post(); ?>
+          <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+          <?php the_content(); ?>
+        <?php endwhile; ?>
+      <?php else: ?>
+        <!-- 投稿データが取得できない場合の処理 -->
+      <?php endif; ?>
+    </div>
+  </div>
+</div>
 
 <footer>
   <div class="container">
