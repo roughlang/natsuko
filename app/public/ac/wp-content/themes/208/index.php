de443496182d60d581c8a90293a0f1699e0297d1
osamu yamakami <osamuyamakami@cpe-172-101-20-100.maine.res.rr.com>
2021-12-20 10:04:32 +0900

    固定ページ作成

--- app/public/ac/wp-content/themes/208/index.php
+++ app/public/ac/wp-content/themes/208/index.php
@@ -203,7 +203,7 @@
           </div>
           <div class="modal-body">
             <p>
-              <img src="/assets/img/top/price/image-08.jpg" width="100%" alt="料金 背中大きめサイズ ¥14,300〜19,800">
+              <img src="/assets/img/top/price/image-09.jpg" width="100%" alt="料金 背中大きめサイズ ¥14,300〜19,800">
               <p class="price-caption">⑧ 背中大きめサイズ ¥14,300〜19,800</p>
             </p>
           </div>
@@ -260,36 +260,4 @@
   </div>
 </div>
 
-<footer class="mt60">
-  <div class="container">
-    <div class="row">
-      <div class="col-sm-4">
-        <h4>Menu</h4>
-        <ul>
-          <li><a href="/ac/#top-price">Price</a></li>
-          <li><a href="http://naturalhigh208.blogspot.jp/">Gallery</a></li>
-          <li><a href="http://www.mehndi.jp/">Contact</a></li>
-        </ul>
-      </div>
-      <div class="col-sm-4">
-      <h4>SNS</h4>
-        <ul>
-          <li><a href="https://twitter.com/Mehndi208">Twitetr</a></li>
-          <li><a href="https://www.instagram.com/natsuko208/?hl=ja">Instagram</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
-      </div>
-      <div class="col-sm-4"> 
-      <h4>Link</h4>
-        <ul>
-          <li><a href="http://mehendi208.blog137.fc2.com/">メヘンディ２０８</a></li>
-          <li><a href="http://naturalhigh208.blogspot.jp/">Natural high</a></li>
-          <li><a href="http://www.mehndi.jp/">ハート＊フール</a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  <p class="copy-right">Copyright (c) Natural Body Art 208 東京 All Rights Reserved.</p>
-</footer>
-</body>
-</html>
+<?php include("inc/footer.php"); ?>
--- app/public/ac/wp-content/themes/208/page.php
+++ app/public/ac/wp-content/themes/208/page.php
@@ -1,1 +1,107 @@
-page
+<?php include("inc/html_header.php"); ?>
+<body>
+
+<?php include("inc/nav.php"); ?>
+
+<div class="container page">
+  <div class="row">
+    <div class="col-md-8 col-lg-9 mt60 page-contents">
+      <div class="page-title mb40">予約 - Reservation -</div>
+      <div class="page-text">
+        <p>
+        当店は東京・国分寺のボディアートサロンです。JR国分寺駅から徒歩１分。
+        ご予約時の最低金額は税抜3000円とさせていただきます。お２人以上での合算でもOKです。
+        </p>
+        <table class="mt20 mb30 price-table">
+          <tbody>
+            <tr>
+              <th> </th>
+              <th>size</th>
+              <th>price</th>
+            </tr>
+            <tr><td>①</td><td>3cm × 3cm以内（2個）</td><td>¥3,300</td></tr>
+            <tr><td>②</td><td>5cm × 5cm</td><td>¥3,300</td></tr>
+            <tr><td>③</td><td>5cm × 10cm</td><td>¥4,400</td></tr>
+            <tr><td>④</td><td>8cm × 8cm</td><td>¥4,400</td></tr>
+            <tr><td>⑤</td><td>8cm × 20cm</td><td>¥8,800</td></tr>
+            <tr><td>⑥</td><td>10cm × 10cm</td><td>¥5,500</td></tr>
+            <tr><td>⑦</td><td>10cm × 20cm</td><td>¥9,900</td></tr>
+            <tr><td>⑧</td><td>背中大きめサイズ</td><td>¥14,300〜19,800</td></tr>
+          </tbody>
+        </table>  
+        <p>
+        ⑨それ以上の大きさは5cm×5cmごとに税抜1000円追加となります。<br>
+        価格は目安です。塗りつぶしの多い柄や緻密な柄は料金が高くなることがあります。<br>
+        ご希望の予算等、ご予約時に是非ご相談ください。
+        </p>
+        <p>
+          ご予約、お問い合わせはこちらから。
+          <script type="text/javascript">
+          <!--
+          function converter(M){
+          var str="", str_as="";
+          for(var i=0;i<M.length;i++){
+          str_as = M.charCodeAt(i);
+          str += String.fromCharCode(str_as + 1);
+          }
+          return str;
+          }
+          var ad = converter(String.fromCharCode(109,45,113,110,110,108,49,47,55,63)+String.fromCharCode(102,108,96,104,107,45,98,110,108));
+          document.write("<a href=\"mai"+"lto:"+ad+"\">"+ad+"<\/a>");
+          //-->
+          </script>
+          <noscript>（画像などを設置）</noscript>
+        </p>
+        <ul>
+          <li>ご希望のお日にちと、時間</li>
+          <li>メヘンディ施術orジャグア施術orレッスン</li>
+          <li>ご希望のサイズ（施術希望の方のみ上記の①～⑧からお選びください）</li>
+          <li>お名前</li>
+          <li>ご予約時間直前まで通じる電話番号</li>
+          <li>持ち込みデザイン(インスタなどに掲載している当店の既存デザイン以外のもの)がある方はこちらにご記入ください。</li>
+        </ul>
+        <p>
+        以上をご記入の上送信してください。<br>
+        どこにどんなデザインを描きたいか、もし決まっていたらそちらもご記入ください。<br>
+        当日までにある程度、描きたいデザインを決めておいてください。<br>
+        持ち込みデザインの場合は必ず事前にご連絡ください。<br>
+        ジャグアタトゥーは施術後１～８時間後にお客様ご自身でインクをシャワーなどで洗い流していただく工程があります。そのようなご予定でご予約ください。<br>
+        お支払いは現金のみとなります。<br>
+        もし９時間を過ぎても返信の無い時は再度 <script type="text/javascript">
+          <!--
+          function converter(M){
+          var str="", str_as="";
+          for(var i=0;i<M.length;i++){
+          str_as = M.charCodeAt(i);
+          str += String.fromCharCode(str_as + 1);
+          }
+          return str;
+          }
+          var ad = converter(String.fromCharCode(109,45,113,110,110,108,49,47,55,63)+String.fromCharCode(102,108,96,104,107,45,98,110,108));
+          document.write("<a href=\"mai"+"lto:"+ad+"\">"+ad+"<\/a>");
+          //-->
+          </script>
+          <noscript>（画像などを設置）</noscript>からご連絡ください。
+        </p>
+        <p class="fs18">
+        Natural body art 208<br>
+        〒185-0012<br>
+        東京都国分寺市本町3-6-12-206<br>
+        </p>
+        <p>
+        ​営業時間は11:00から22:00、不定休です。
+        </p>
+
+
+      </div>
+    </div>
+    <div class="col-md-2 col-lg-3 mt60 sidebar">
+      <h2 class="sidebar-title">sidebar</h2>
+      <div class="">
+      sidebar
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php include("inc/footer.php"); ?>
